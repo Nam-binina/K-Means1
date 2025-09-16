841f6d27f6c8ff56e176eb38fe7448b0643a5fd9
Nam-binina <bryanrakotomananandro@gmail.com>
2025-09-16 14:46:00 +0300

    Update kmeans.py

---
+++ kmeans.py
@@ -0,0 +1,44 @@
+import numpy as np
+import random
+import math
+
+epsilon = 1e-6
+
+import random
+
+def firstcenter(points, ncenters):
+    center = []
+    for i in range(ncenters):
+        center.append(points[random.randint(0, len(points)-1)])
+    return center
+
+
+def distance(point1,point2):
+    return math.sqrt(math.pow(point1[0]-point2[0],2)+math.pow(point1[1]-point2[1],2))
+
+def convergence(oldcenter,newcenter):
+    if distance(oldcenter[0],oldcenter[1])+distance(newcenter[0],newcenter[1])<epsilon:
+        return False
+    return True
+def center(points):
+    x = sum(points[0] for _ in points) / len(points)
+    y = sum(points[1] for _ in points) / len(points)    
+    return x,y
+    
+    
+
+def kmeans(points,ncenter):
+    centers = firstcenter(points,ncenter)
+    optimal = True
+    array = centers.copy()
+    while optimal:
+        temparray = [[] for _ in range(ncenter)]
+        for i in range(0,len(points)):
+            if points[i] not in centers:
+                distances = [distance(i, c) for c in centers]  # distance à définir (ex: euclidienne)
+                min_index = distances.index(min(distances))
+                temparray[min_index].append(i)
+            newcenter = [center(j) for j in temparray]
+        optimal = convergence(center,newcenter)
+        center = newcenter
+    return array
